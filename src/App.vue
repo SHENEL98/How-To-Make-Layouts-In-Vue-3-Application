f09f2c277de89fe9353e29fb58b5429da93a4cb9
SHENEL98 <wsakurera@gmail.com>
2023-08-19 12:04:34 +0200

    second test

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <ul>
       <li><router-link :to="{ name: 'HelloWorld' }"> HelloWorld </router-link></li>
       <li><router-link :to="{ name: 'Home' }"> Home </router-link></li>
-
     </ul>
   </nav>
   <router-view />
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HelloWorld from "/src/components/HelloWorld.vue";
 import Home from "/src/components/IndexHome.vue";
+import DefaultLayout from '/src/layouts/DefaultLayout.vue';
 
 const routes = [
   {
     path: "/",
-    name: "HelloWorld",
-    component: HelloWorld,
+    name: "Public",
+    component: DefaultLayout,
+    redirect:'/',
+    children:[
+      {
+        path:'/',
+        name:'Home',
+        component:Home
+      }
+    ]
   },
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "HelloWorld",
+    component: HelloWorld,
   },
   
 ];
